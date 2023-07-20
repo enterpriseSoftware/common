addbd08f201cb642af767aeb8f8a149c777ba95a
Tim Kelley <abelwebsolutions@gmail.com>
2023-07-19 19:10:22 -0500

    Updates

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from "express";
-import { NotAuthorizedError } from "../error/not-authorized-error";
+import { Request, Response, NextFunction } from 'express';
+import { NotAuthorizedError } from '../error/not-authorized-error';
 
 export const requireAuth = (
   req: Request,
@@ -9,5 +9,6 @@ export const requireAuth = (
   if (!req.currentUser) {
     throw new NotAuthorizedError();
   }
+
   next();
 };
